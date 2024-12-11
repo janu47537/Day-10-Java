3b32a87acf970ebbeef79927023c894aef5bd1b6
A Janani Priya <janu47537@gmail.com>
2024-12-11 13:26:54 +0530

    Add files via upload

---
+++ withoutmodifier.java
@@ -0,0 +1,10 @@
+class withoutmodifier{
+    static String s()
+    {
+        return "Hello Students";
+    }
+    public static void main(String[]args){
+        String res=s();
+        System.out.println(res);
+    }
+}
