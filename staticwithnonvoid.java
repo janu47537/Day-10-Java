9895d9bfee7842a7b0a527b0d037c2080001bfa9
A Janani Priya <janu47537@gmail.com>
2024-12-11 13:26:15 +0530

    Add files via upload

---
+++ staticwithnonvoid.java
@@ -0,0 +1,10 @@
+class staticwithnonvoid{
+    public static String s()
+    {
+        return "Hello Students";
+    }
+    public static void main(String[]args){
+        String res=s();
+        System.out.println(res);
+    }
+}
