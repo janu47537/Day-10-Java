0adc6edfedcc015ba4a567c65d3169dfc0745445
A Janani Priya <janu47537@gmail.com>
2024-12-11 15:35:17 +0530

    Add files via upload

---
+++ Defaultconstructor.java
@@ -0,0 +1,23 @@
+ class Main{
+    int c;
+    String b;
+    public void a()
+    {
+        this.c=345;
+        this.b="Hello Ram";
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+}
+  public class Defaultconstructor{
+    public static void main(String[]args){
+    Main ob=new Main();
+    ob.a();
+    ob.display();
+    }
+}
+
+          
+          
